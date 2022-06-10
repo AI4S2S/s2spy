444fe89c2bf7c881a8beab4a88b0ccc7d84b9318
Yang <y.liu@esciencecenter.nl>
2022-06-10 13:41:09 +0200

    remove out of date resampling and map to data methods

--- tests/test_time.py
+++ tests/test_time.py
@@ -58,51 +58,6 @@ class TestAdventCalendar:
         )
         assert np.array_equal(years, expected)
 
-    def test_map_year_to_data(self):
-        cal = AdventCalendar(anchor_date=(12, 31), freq="180d")
-        # single year in input data
-        time_index = pd.date_range('20210401', '20211231', freq='180d')
-        test_data = np.random.random(len(time_index))
-        timeseries = pd.Series(test_data, index=time_index)
-        year = cal.map_year_to_data(timeseries)
-
-        expected = np.array(
-            [
-                interval("2021-07-04", "2021-12-31"),
-                interval("2021-01-05", "2021-07-04"),
-            ]
-        )
-
-        assert np.array_equal(year, expected)
-
-        # multiple years in input data
-        time_index = pd.date_range('20200401', '20211231', freq='180d')
-        test_data = np.random.random(len(time_index))
-
-        # timeindex normal order
-        timeseries = pd.Series(test_data, index=time_index)
-        years = cal.map_year_to_data(timeseries)
-        expected = np.array(
-            [
-                [
-                    interval("2021-07-04", "2021-12-31"),
-                    interval("2021-01-05", "2021-07-04"),
-                ],
-                [
-                    interval("2020-07-04", "2020-12-31"),
-                    interval("2020-01-06", "2020-07-04"),  # notice the leap day
-                ],
-            ]
-        )
-
-        assert np.array_equal(years, expected)
-
-        # timeindex reverse order
-        timeseries = pd.Series(test_data, index=time_index[::-1])
-        years = cal.map_year_to_data(timeseries)
-
-        assert np.array_equal(years, expected)
-
     def test_mark_target_period(self):
         cal = AdventCalendar()
 
@@ -117,32 +72,6 @@ class TestAdventCalendar:
 
         with pytest.raises(ValueError):
             cal.mark_target_period(end="20200101")
-
-    def test_resample_with_dataframe(self):
-        cal = AdventCalendar()
-        time_index = pd.date_range('20211101', '20211116', freq='1d')
-        test_data = np.arange(0, 16, 1)
-
-        # timeindex normal order
-        timeseries = pd.Series(test_data, index=time_index)
-        bins = cal.resample(timeseries, target_freq='5d')
-        # resample hand calculation
-        expected = np.mean(test_data[:15].reshape(-1, 5), axis=1)
-        expected = np.append(expected, test_data[-1])
-
-        assert np.array_equal(bins, expected)
-
-        # timeindex reverse order
-        dataframe = pd.DataFrame({"var_1": test_data[::-1], "var_2": test_data}, index=time_index[::-1])
-        bins = cal.resample(dataframe, target_freq='5d')
-        # resample hand calculation
-        test_data_array = np.array([test_data[::-1], test_data]).T
-        expected = np.mean(test_data_array[:15, :].reshape(-1, 5, 2), axis=1)
-        expected = np.append(expected, test_data_array[-1, :].reshape(-1, 2), axis=0)
-
-        assert np.array_equal(bins, expected)
-
-    # def test_resample_with_dataarray(self):
 
     def test_get_lagged_indices(self):
         cal = AdventCalendar()
