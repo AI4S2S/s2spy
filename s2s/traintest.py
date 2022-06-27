443654fe00155c0ba9eb30e11b97193d62fdea2f
Yang <y.liu@esciencecenter.nl>
2022-06-27 14:57:25 +0200

    complete kfold method

--- s2s/traintest.py
+++ s2s/traintest.py
@@ -2,15 +2,37 @@
 
 A collection of train/test splitting approaches.
 """
-#from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold
+import numpy as np
 import pandas as pd
 
 
-def kfold(intervals_df: pd.DataFrame, **kwargs):
+def kfold(intervals_df: pd.DataFrame, n_splits: int=2, **kwargs):
     """
     K-Folds cross-validator, which splits provided intervals into k
     consecutive folds.
+
+    args:
+        intervals_df: DataFrame to store the train/test groups.
+        kfold: number of train/test splits.
+        # To be implemented.
+        Gap_prior: skip n groups/years prior to every test group/year
+        Gap_after: skip n groups/years after every test group/year     
     """
+    # instantiate kfold cross-validator
+    cv = KFold(n_splits, **kwargs)
+    anchor_years = np.unique(intervals_df.anchor_year.values)
+    folds = cv.split(anchor_years)
+
+    for i, (train_index, test_index) in enumerate(folds):
+        # get train year index and test year index
+        train_years = [anchor_years[index] for index in train_index]
+        test_years = [anchor_years[index] for index in test_index]
+        # label train/test group
+        col_name = 'fold_' + str(i)
+        intervals_df[col_name] = 'skip'
+        intervals_df.loc[intervals_df['anchor_year'].isin(train_years), col_name] = 'train'
+        intervals_df.loc[intervals_df['anchor_year'].isin(test_years), col_name] = 'test'
 
     return intervals_df
 
