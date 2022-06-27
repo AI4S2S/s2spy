201f2c227a7fffcfeb02c0382d41a300301e99d2
Yang <y.liu@esciencecenter.nl>
2022-06-27 17:06:14 +0200

    add doc strings to kfold function

--- s2s/traintest.py
+++ s2s/traintest.py
@@ -12,12 +12,20 @@ def kfold(intervals_df: pd.DataFrame, n_splits: int=2, **kwargs):
     K-Folds cross-validator, which splits provided intervals into k
     consecutive folds.
 
+    For more information
+
     args:
         intervals_df: DataFrame to store the train/test groups.
-        kfold: number of train/test splits.
-        # To be implemented.
+        n_splits: number of train/test splits.
+        # To do
         Gap_prior: skip n groups/years prior to every test group/year
-        Gap_after: skip n groups/years after every test group/year     
+        Gap_after: skip n groups/years after every test group/year
+
+    Other keyword arguments: see the documentation for sklearn.model_selection.KFold:
+    https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
+
+    Returns:
+        Pandas DataFrame with columns containing train/test label in each fold.
     """
     # instantiate kfold cross-validator
     cv = KFold(n_splits, **kwargs)
