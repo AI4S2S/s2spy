2a6868af8f9801a6607a151d72d111e787b76b29
Yang <y.liu@esciencecenter.nl>
2022-07-14 17:48:31 +0200

    add sub-modules to RGDR module

---
+++ s2spy/RGDR/__init__.py
@@ -0,0 +1,14 @@
+"""Response Guided Dimensionality Reduction."""
+from . import map_analysis
+from . import map_regions
+
+
+def operator(series, fields):
+    """Operator for lag shifting and execute RGDR functions.
+    
+    Invoke functions to compute correlations and to perform clustering.
+    """
+    # To do: call `map_analysis` and `map_regions` and manage
+    #  inputs and outputs.
+    raise NotImplementedError
+
---
+++ s2spy/RGDR/map_regions.py
@@ -0,0 +1,20 @@
+"""Regions clustering utils.
+
+A module for clustering regions based on the given correlation
+between spatial fields and target timeseries.
+"""
+
+def spatial_mean():
+    """Calculate 1-d timeseries for each precursor region. Precursor regions 
+    are integer label masks within the np.ndarray labels.
+    """
+    raise NotImplementedError
+
+def cluster_DBSCAN():
+    """Perform DBSCAN clustering from vector array or distance matrix.
+
+    Density-Based Spatial Clustering of Applications with Noise (DBSCAN).
+    Clusters gridcells together which are of the same sign and in proximity to
+    each other using DBSCAN.
+    """
+    raise NotImplementedError
