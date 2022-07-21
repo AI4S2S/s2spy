e1058d4bcdf2ce1842ff403640e9214115542016
Yang <y.liu@esciencecenter.nl>
2022-07-21 19:29:11 +0200

    update to do list

--- s2spy/rgdr/_map_regions.py
+++ s2spy/rgdr/_map_regions.py
@@ -3,8 +3,6 @@
 A module for clustering regions based on the given correlation
 between spatial fields and target timeseries.
 """
-import itertools
-from math import radians
 import numpy as np
 from sklearn.cluster import DBSCAN
 
@@ -22,18 +20,22 @@ def cluster_dbscan(
     The input data will be processed in this function to ensure that the distance
     is free of the impact from spherical curvature. The actual Euclidean distance
     will be obtained and passed to the DBSCAN clustering function.
+
+
     """
     # To do: check if the longitude is between -180 : 180
     # To do: input with lags dimension to vectorize dbscan and keep labels
-    # To do: handle signs for +/- correlation separately
+    # To do: handle clustering for +/- correlation separately
     # To do: mask data using significance (p_value)
     # To do: check if it is necessary to have weight by area
     #  (the impact of area is already reflected by the haversine distance)
+    # To do: add relevant checks for the results and raise warnings
+    #  (e.g. check whether the results are noise or not)
     # To do: calculate and verify DBSCAN
     mask = map_analysis.p_values.values < threshold
     grid_lon, grid_lat = np.meshgrid((map_analysis.longitude.values + 180) % 360 - 180,
         map_analysis.latitude.values)
-    #grids = [[np.radians(grid_lat.reshape(-1)), np.radians(grid_lon.reshape(-1))]]
+    # prepare [lat, lon] pairs as input for DBSCAN
     grid_pair_up = [[lat, lon] for lat, lon in zip(np.radians(grid_lat.reshape(-1)),
         np.radians(grid_lon.reshape(-1)))]
     cluster = DBSCAN(eps = eps, min_samples = min_samples, metric = 'haversine',
