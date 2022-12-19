3256182ccbc76a003c078898d95682a9607a0f78
jannesvaningen <82503135+jannesvaningen@users.noreply.github.com>
2022-12-19 17:09:07 +0100

    added staggered calendar and tests

--- s2spy/calendar_shifter.py
+++ s2spy/calendar_shifter.py
@@ -4,91 +4,24 @@ import xarray as xr
 import s2spy.time
 from s2spy.time import resample  # pylint: disable=unused-import
 
-# pylint: disable=too-many-arguments
-def cal_builder(
-    anchor,
-    target_length,
-    precur_length,
-    n_targetperiods=1,
-    n_precurperiods=1,
-    target_overlap="0d",
-    precur_overlap="0d",
-    precur_leadtime="0d",
-):
-    """
-    Build a calendar that resembles a typical NWP-type S2S forecast. Typically contains:
-        - One or more targetperiods, for example the 1st, 2nd, 3rd and 4th week of June
-        - One or more precursorperiods, for example the weeks preceeding the target
-        periods
-        - A leadtime for the precursors. A forecast run is typically done >2 weeks up to
-        7 months ahead
-    This timeframe is usually repeated on a regular basis as new data comes in. For
-    example, every first of the month the forecast is reinitiated.
-    Args:
-        anchor_date: the referral date of your calendar. This is the first day of the
-            first target period.
-        target_length: length of a target period
-        precur_length: length of a precursor period
-        n_targetperiods: number of targetperiods
-        target_overlap: overlap between target periods
-        n_precurperiods: number of precursorperiods
-        precur_overlap: overlap between precursor periods
-        precur_leadtime: gap (leadtime) between first target (i_interval = 0) and
-            first precursor period (i_interval = -1)
-    Returns:
-        A calendar
 
-    Example:
-        >>> import s2spy.time
-        >>> cal = nwp_calendar_builder(start_date, '7d', '7d', precur_leadtime='14d', n_precurperiods=4)
-        >>> cal
-        Calendar(
-        anchor='07-01',
-        allow_overlap=False,
-        mapping=None,
-        intervals=[
-            Interval(role='target', length='7d', gap='0d'),
-            Interval(role='precursor', length='7d', gap='14d'),
-            Interval(role='precursor', length='7d', gap='0d'),
-            Interval(role='precursor', length='7d', gap='0d'),
-            Interval(role='precursor', length='7d', gap='0d'),
-            ]
-        )
-    """
-    # create calendar instance
-    cal = s2spy.time.Calendar(anchor=anchor_date)
-    # add target periods to calendar
-    for i in range(n_targetperiods):
-        if i == 0:  # don't add overlap for first targetperiod = shifting
-            cal.add_interval("target", length=target_length)
-        else:  # overlap
-            cal.add_interval("target", length=target_length, gap=target_overlap)
-    # add precursor periods to calendar
-    for j in range(n_precurperiods):
-        if j == 0:  # add leadtime for first precurperiod
-            cal.add_interval("precursor", length=precur_length, gap=precur_leadtime)
-        else:  # overlap
-            cal.add_interval("precursor", length=precur_length, gap=precur_overlap)
-
-    return cal
-
-
-def _gap_shift(interval: s2spy.time.Interval, shift: Union[str, dict[str, int]]) -> dict[str, int]:
+def _gap_shift(
+    interval: s2spy.time.Interval, shift: Union[str, dict[str, int]]
+) -> dict[str, int]:
     """
     Shift a gap from a calendar interval by an input of a pandas-like
     frequency string (e.g. "10d", "2W", or "3M"), or a pandas.DateOffset
     compatible dictionary such as {days=10}, {weeks=2}, or {months=1, weeks=2}..
     Args:
-        - gap = a pandas DateOffset from a calendar instance.
-        - shift = the shift for the gap
+        gap: a pandas DateOffset from a calendar instance.
+        shift: the shift for the gap
     Returns:
-        - dict with gap offset by shift
+        dict with gap offset by shift
     Example:
         >>> import s2spy.time
-        >>> gap_shifted = gap_shift()
-        >>> cal = nwp_calendar_builder(start_date, '7d', '7d', precur_leadtime='14d', n_precurperiods=4)
-        >>> interval = cal.targets[0]
-        >>> gap_shifted = gap_shift(interval, '7d')
+        >>> cal = s2spy.time.Calendar(anchor='07-01')
+        >>> cal.add_interval("target", "7d")
+        >>> gap_shifted = _gap_shift(cal.targets[0], '7d')
         >>> gap_shifted
         {'days': 7}
     """
@@ -98,7 +31,10 @@ def _gap_shift(interval: s2spy.time.Interval, shift: Union[str, dict[str, int]])
     else:
         gap_time_dict = interval.gap
     # pylint: disable=protected-access
-    shift_time_dict = interval._parse_timestring(shift)
+    if isinstance(shift, str):
+        shift_time_dict = interval._parse_timestring(shift)
+    else:
+        shift_time_dict = shift
     # make the shift negative for the precursor to shift forward in time
     if interval.role == "precursor":
         shift_time_dict.update(
@@ -113,29 +49,33 @@ def _gap_shift(interval: s2spy.time.Interval, shift: Union[str, dict[str, int]])
     return gap_shifted
 
 
-def cal_shifter(
+def calendar_shifter(
     calendar: s2spy.time.Calendar, shift: Union[str, dict]
 ) -> s2spy.time.Calendar:
     """
     Shift a Calendar instance by a given time offset. Instead of shifting the
     anchor date, this function shifts two things in reference to the anchor date:
-    - the target period(s): as a gap between the anchor date and the start of the first
-        target period
-    - the precursor period(s): as a gap between the anchor date and the start of the
-        first precursor period
+        target period(s): as a gap between the anchor date and the start of the first
+            target period
+        precursor period(s): as a gap between the anchor date and the start of the
+            first precursor period
     This way, the anchor year from the input calendar is maintained on the returned
     calendar. This is important for train-test splitting at later stages.
-    - Args:
-        - calendar : a s2spy.time.Calendar instance
-        - shift: a pandas-like
-        frequency string (e.g. "10d", "2W", or "3M"), or a pandas.DateOffset
-        compatible dictionary such as {days=10}, {weeks=2}, or {months=1, weeks=2}
-    - Example:
+    Args:
+        calendar: a s2spy.time.Calendar instance
+        shift: a pandas-like
+            frequency string (e.g. "10d", "2W", or "3M"), or a pandas.DateOffset
+            compatible dictionary such as {days=10}, {weeks=2}, or {months=1, weeks=2}
+    Example:
         Shift a calendar by a given dateoffset.
         >>> import s2spy.time
-        >>> gap_shifted = gap_shift()
-        >>> cal = nwp_calendar_builder(start_date, '7d', '7d', precur_leadtime='14d', n_precurperiods=4)
-        >>> cal_shifted = shifter(cal, '7d')
+        >>> cal = s2spy.time.Calendar(anchor='07-01')
+        >>> cal.add_interval("target", "7d")
+        >>> cal.add_interval("precursor", "7d", gap="14d")
+        >>> for _ in range(3):
+                cal.add_interval("precursor", "7d")
+        >>> cal_shifted = cal_shifter(cal, '7d')
+        >>> cal_shifted
         Calendar(
             anchor='07-01',
             allow_overlap=False,
@@ -156,17 +96,82 @@ def cal_shifter(
     return calendar_shifted
 
 
-def cal_list_resampler(cal_list: list, ds: xr.Dataset) -> xr.DataArray:
+def staggered_calendar(
+    calendar: s2spy.time.Calendar, shift: Union[str, dict], n_shifts: int
+) -> s2spy.time.Calendar:
+    """
+    Shift a Calendar instance by a given time offset n times to create a list of shifted
+    calendars. We call this a list a staggered calendar.
+    Args:
+        calendar: an s2spy.time.Calendar instance
+        shift: a pandas-like
+            frequency string (e.g. "10d", "2W", or "3M"), or a pandas.DateOffset
+            compatible dictionary such as {days=10}, {weeks=2}, or {months=1, weeks=2}
+        n_shifts: strictly positive integer for the number of shifts
+    Example:
+        Shift an input calendar n times by a given dateoffset and return a list of these
+        shifted calendars.
+        >>> import s2spy.time
+        >>> cal = s2spy.time.Calendar(anchor='07-01')
+        >>> cal.add_interval("target", "7d")
+        >>> cal.add_interval("precursor", "7d", gap="14d")
+        >>> for _ in range(3):
+                cal.add_interval("precursor", "7d")
+        >>> cal_shifted = cal_stagger(cal, '7d', 1)
+        >>> cal_shifted
+        [Calendar(
+            anchor='07-01',
+            allow_overlap=False,
+            mapping=None,
+            intervals=[
+                Interval(role='target', length='7d', gap='0d'),
+                Interval(role='precursor', length='7d', gap='14d'),
+                Interval(role='precursor', length='7d', gap='0d'),
+                Interval(role='precursor', length='7d', gap='0d'),
+                Interval(role='precursor', length='7d', gap='0d')
+            ]
+        ),
+        Calendar(
+            anchor='07-01',
+            allow_overlap=False,
+            mapping=None,
+            intervals=[
+                Interval(role='target', length='7d', gap={'days': 7}),
+                Interval(role='precursor', length='7d', gap={'days': 7}),
+                Interval(role='precursor', length='7d', gap='0d'),
+                Interval(role='precursor', length='7d', gap='0d'),
+                Interval(role='precursor', length='7d', gap='0d')
+            ]
+        )]
+    """
+    if not isinstance(n_shifts, int):
+        raise ValueError(
+            "Please input an 'int' type for the 'n' argument."
+            f" Not a {type(n_shifts)}."
+        )
+    if n_shifts <= 0:
+        raise ValueError(
+            "The number of shifts 'n' has to be 1 or greater, " f"not '{n_shifts}'."
+        )
+    cal_staggered = [calendar]
+    for _ in range(n_shifts):
+        cal_shifted = s2spy.calendar_shifter.calendar_shifter(cal_staggered[-1], shift)
+        cal_staggered.append(cal_shifted)
+
+    return cal_staggered
+
+
+def calendar_list_resampler(cal_list: list, ds: xr.Dataset) -> xr.Dataset:
     """
     Resample a dataset to every calendar in a list of calendars and concatenate them
-    to a new dataset with dimension 'run'.
-    - Args:
-        - cal_list : list of shifted custom calendars
-        - ds: dataset to resample
+    to a new xr.Dataset with dimension 'run'.
+    Args:
+        cal_list: list of shifted custom calendars
+        ds: dataset to resample
+    Returns:
+        resampled xr.Dataset
     """
-    # loop over calendar in the nwp_calendar_list
     ds_r = xr.concat([s2spy.time.resample(cal, ds) for cal in cal_list], dim="run")
-    # add coordinates for dimension 'run'
     ds_r = ds_r.assign_coords({"run": ds_r.run.values})
 
     return ds_r
--- tests/test_calendar_shifter.py
+++ tests/test_calendar_shifter.py
@@ -16,43 +16,18 @@ class TestCalendarShifter:
     @pytest.fixture(autouse=True)
     def dummy_calendar(self):
         cal = Calendar(anchor="12-31")
-        # append building blocks
         cal.add_interval("target", "7d")
         cal.add_interval("precursor", "7d", "7d")
         cal.add_interval("precursor", "7d")
         return cal
 
-    def test_nwp_calendar_builder(self, dummy_calendar):
-        cal = calendar_shifter.cal_builder(
-            anchor_date="12-31",
-            target_length="7d",
-            precur_length="7d",
-            n_precurperiods=2,
-            precur_leadtime="7d",
-        )
-        expected = dummy_calendar
-        assert np.array_equal(cal, expected)
-    
-    def test_shifter(self, dummy_calendar):
-        shift = {'days':7}
-        cal_shifted = calendar_shifter.cal_shifter(dummy_calendar, shift)
-        cal_expected = copy.deepcopy(dummy_calendar)
-        cal_expected.targets[0].gap = shift
-        cal_expected.precursors[0].gap = {(key, value * -1) for key, value in shift.items()}
-        assert np.array_equal(cal_shifted, cal_expected)
-
     @pytest.fixture(autouse=True)
-    def dummy_data():
-
-        time_index = pd.date_range('20211201', '20220131', freq='1d')
-
+    def dummy_data(self):
+        time_index = pd.date_range("20211001", "20220501", freq="1d")
         np.random.seed(0)
         temperature = 15 + 8 * np.random.randn(2, 2, len(time_index))
-
         lon = [[-99.83, -99.32], [-99.79, -99.23]]
         lat = [[42.25, 42.21], [42.63, 42.59]]
-
-
         ds = xr.Dataset(
             data_vars=dict(
                 temperature=(["x", "y", "time"], temperature),
@@ -66,16 +41,29 @@ class TestCalendarShifter:
         )
         return ds
 
-    def test_cal_list_resampler(self, dummy_calendar, dummy_data):
-        #generate cal_list
-        n_runs = 3
-        shift = '7d'
-        cal_list = [dummy_calendar]
-        for _ in range(n_runs - 1):
-            cal_shifted = calendar_shifter.cal_shifter(cal_list[-1], shift)
-            cal_list.append(cal_shifted)
-        #use list resampler
-        ds_resampled = calendar_shifter.cal_list_resampler(cal_list, dummy_data)
-        #generate expected
-        ds_expected = 
-        assert np.array_equal(ds_resampled, ds_expected)
+    def test_calendar_shifter(self, dummy_calendar):
+        shift = {"days": 7}
+        cal_shifted = calendar_shifter.calendar_shifter(dummy_calendar, shift)
+        cal_expected = copy.deepcopy(dummy_calendar)
+        cal_expected.targets[0].gap = {"days": 7}
+        cal_expected.precursors[0].gap = {"days": 0}
+        # pytest.set_trace()
+        assert len(cal_shifted.targets + cal_shifted.precursors) == len(
+            cal_expected.targets + cal_expected.precursors
+        )
+        assert str(cal_shifted.targets) == str(cal_expected.targets)
+        assert str(cal_expected.precursors) == str(cal_shifted.precursors)
+
+    def test_staggered_calendar(self, dummy_calendar):
+        cal_list = calendar_shifter.staggered_calendar(dummy_calendar, "7d", n_shifts=3)
+        assert len(cal_list) == 4
+
+    def test_calendar_list_resampler(self, dummy_calendar, dummy_data):
+        cal_list = calendar_shifter.staggered_calendar(
+            dummy_calendar, "14d", n_shifts=5
+        )
+        for cal in cal_list:
+            cal.map_to_data(dummy_data)
+        ds_resampled = calendar_shifter.calendar_list_resampler(cal_list, dummy_data)
+        expected_coords = np.array([0, 1, 2, 3, 4, 5])
+        assert np.array_equal(ds_resampled.run.values, expected_coords)
