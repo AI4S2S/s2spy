a299d6b33256c855e4803a6c464060f0f979b2ae
Yang <y.liu@esciencecenter.nl>
2023-09-13 14:59:40 +0200

    bumpversion minor

--- s2spy/__init__.py
+++ s2spy/__init__.py
@@ -11,6 +11,6 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Yang Liu"
 __email__ = "y.liu@esciencecenter.nl"
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 __all__ = ["RGDR"]
